f318e15866bf944feb07af152d557e6b9f7855c1
Jesús Gaspariano <cuquin776@gmail.com>
2026-06-27 11:03:18 -0600

    feat: implementación de las 3 versiones en Ruby

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 "# soap-multilenguaje" 
+
+## 1. Ruby
+
+### Requisitos
+* Ruby instalado.
+* Ejecutar `bundle install` para instalar las dependencias (Sinatra, Savon, GoogleTranslateDiff, NumbersAndWords).
+
+### Ejecución
+Ejecutar el siguiente comando en la terminal:
+`ruby app.rb`
+
+* **Punto 1 (SOAP):** http://localhost:8000/clisoap1?n=10
+* **Punto 2 (Traducción):** http://localhost:8000/clisoap2?n=10
+* **Punto 3 (Nativo):** http://localhost:8000/conintl?n=10
---
+++ app.rb
@@ -0,0 +1,62 @@
+require 'sinatra'
+require 'savon'
+require 'google_translate_diff'
+require 'numbers_and_words'
+
+# Configurar el puerto para que sea igual al de tu ejemplo en PHP
+set :port, 8000
+
+# 1. Consumir SOAP público
+# http://localhost:8000/clisoap1?n=10
+get '/clisoap1' do
+  numero = params['n'] || 10
+  
+  # Inicializar cliente SOAP
+ cliente = Savon.client(
+    wsdl: 'http://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL',
+    adapter: :httpclient, # Usamos el nuevo motor de red
+    open_timeout: 60,
+    read_timeout: 60,
+    ssl_verify_mode: :none
+  )
+  
+  # Llamar al método NumberToWords
+  respuesta = cliente.call(:number_to_words, message: { 'ubiNum' => numero })
+  
+  # Extraer el resultado
+  respuesta.body[:number_to_words_response][:number_to_words_result]
+end
+
+# 2. Consumir SOAP y Traducir de inglés a español
+# http://localhost:8000/clisoap2?n=10
+get '/clisoap2' do
+  numero = params['n'] || 10
+  
+  cliente = Savon.client(
+    wsdl: 'http://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL',
+    adapter: :httpclient, # Usamos el nuevo motor de red
+    open_timeout: 60,
+    read_timeout: 60,
+    ssl_verify_mode: :none
+  )
+  respuesta = cliente.call(:number_to_words, message: { 'ubiNum' => numero })
+  texto_ingles = respuesta.body[:number_to_words_response][:number_to_words_result]
+  
+  # Configurar el traductor (origen: en, destino: es)
+  traductor = GoogleTranslateDiff::LinearTranslator.new('en', 'es')
+  resultado_espanol = traductor.translate(texto_ingles)
+  
+  resultado_espanol
+end
+
+# 3. Convertir número a letras con librería nativa
+# http://localhost:8000/conintl?n=10
+get '/conintl' do
+  numero = params['n'].to_i || 10
+  
+  # Configurar la librería al idioma español
+  I18n.locale = :es
+  
+  # Convertir usando el método que la librería inyecta a los números enteros
+  numero.to_words
+end
